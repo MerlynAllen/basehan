1e2bbe72331f6e18c4a4c251d8301a580c5a7115
Merlyn Allen <me@xiaorui.li>
2022-10-19 00:22:21 +0800

    Init commit. Complete with the first version, no stream processing.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-pub mod BaseHan {
+pub mod base_han {
     pub const BASE_OFFSET: u32 = 0x4e00;
-    pub const MULTIBYTE_SIGN: u32 = 0x4dff;
+    pub const MULTIBYTE_SIGN: u32 = 0x8e00;
     pub fn encode(raw: Vec<u8>) -> Vec<char> {
         let mut result = Vec::new();
         let mut buff = 0u32;
@@ -38,14 +38,14 @@ pub mod BaseHan {
         if is_multibyte_tail {
             basehan.pop();
         }
-        let total_bytes = basehan.len() * 13 / 8 - if is_multibyte_tail { 0 } else { 1 };
-        let tail_bits = total_bytes * 8 % 13;
+        // let total_bytes = basehan.len() * 13 / 8 - if is_multibyte_tail { 0 } else { 1 };
+        // let tail_bits = total_bytes * 8 % 13;
         let mut result = Vec::new();
         let mut buff = 0u32;
         let mut bit_pointer = 0;
 
         for i in 0..basehan.len() - 1 {
-            let mut index = basehan[i] as u32 - BASE_OFFSET;
+            let index = basehan[i] as u32 - BASE_OFFSET;
             buff = (buff << 13) | (index & 0x1FFF);
             bit_pointer += 13;
             while bit_pointer >= 8 {
@@ -54,10 +54,17 @@ pub mod BaseHan {
                 result.push(byte as u8);
             }
         }
-        // last byte
-        let index = basehan[basehan.len() - 1] as u32 - BASE_OFFSET;
+        // last byte(s)
+        let tail_bits = (8 * if is_multibyte_tail { 2 } else { 1 }) - bit_pointer;
+
+        let index = (basehan[basehan.len() - 1] as i32 - BASE_OFFSET as i32) as u32;
         buff = (buff << tail_bits) | (index & (0xFFFFFFFF >> (32 - tail_bits)));
-        assert!(bit_pointer + tail_bits == 8);
+        // assert!((bit_pointer + tail_bits) % 8 == 0);
+        if is_multibyte_tail {
+            let byte = (buff >> 8) & 0xFF;
+            result.push(byte as u8);
+            buff = buff >> 8;
+        }
         let byte = buff & 0xFF;
         result.push(byte as u8);
         result
@@ -71,7 +78,7 @@ use std::io::{self, stdin, Stdin, Read, Write};
 #[command(author, about, version)]
 struct Args {
     // Whether encode or docode
-    #[clap(short, long, default_value="true")]
+    #[clap(short, long, default_value="false")]
     decode: bool,
 }
 
@@ -80,11 +87,11 @@ fn main() {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer).unwrap();
     if args.decode {
-        let result = BaseHan::decode(buffer);
+        let result = base_han::decode(buffer);
         let result = String::from_utf8_lossy(result.as_slice()).to_string();
         io::stdout().write_all(result.as_bytes()).unwrap();
     } else {
-        let result = BaseHan::encode(buffer.as_bytes().to_vec());
+        let result = base_han::encode(buffer.as_bytes().to_vec());
         io::stdout().write_all(result.iter().collect::<String>().as_bytes()).unwrap();
     }
 }
